6a8ec692fdd5944895d788c34ce108e59b4af75d
YutaIto <n88.ito.yuta@gmail.com>
2021-12-12 05:34:03 +0900

    ToppagesControllerの作成とトップページ表示完了

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// プレビューをした瞬間の設定
+Route::get('/', 'ToppagesController@index');
